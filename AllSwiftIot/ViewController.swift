bb1da0056b9a77538ec109495ed138b45b39b635
Satoshi SAKAO <ottijp@users.noreply.github.com>
2019-01-19 14:40:16 +0900

    Initial Commit

---
+++ AllSwiftIot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AllSwiftIot
+//
+//  Created by otti on 2019/01/19.
+//  Copyright © 2019 otti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
